1162ac3b8feb8c42bf419ee9e776e3e221e86231
banxiafire <104917595+banxiafire@users.noreply.github.com>
2024-10-21 18:06:09 -0400

    Task 2.1 done

--- src/main/java/app/MainWithInMemory.java
+++ src/main/java/app/MainWithInMemory.java
@@ -1,9 +1,10 @@
 package app;
 
+import java.awt.CardLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
-import java.awt.*;
 
 import data_access.InMemoryUserDataAccessObject;
 import interface_adapter.ViewManagerModel;
